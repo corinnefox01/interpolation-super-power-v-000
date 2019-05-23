4ad206d700458967d6cb9856c7ea40b473841a70
Corinne Fox <corinnefox01@gmail.com>
2019-05-23 15:09:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,11 @@ def display_rainbow (colors)
 end
 
 colors = ["R", "O" ,"Y", "G", "B", "I", "V"]
+
+puts "R: #{red[0]}"
+puts "O: #{orange[1]}"
+puts "Y: #{yellow[2]}"
+puts "G: #{green[3]}"
+puts "B: #{blue[4]}"
+puts "I: #{indigo[5]}"
+puts "V: #{violet[6]}"
